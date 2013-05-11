58f5d91ece4e2dbc13abcae5f056b84816e959c1
Jenna Gopilan <jgopilan@gmail.com>
2013-05-11 16:24:17 -0400

    Week 2 submission

--- chap6.py
+++ chap6.py
@@ -1,9 +1,56 @@
-# Enter your answrs for chapter 6 here
-# Name:
+# Enter your answers for chapter 6 here
+# Name: Jenna Gopilan
 
 
 # Ex. 6.6
 
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
+def is_palindrome(word):
+    """
+    Determines if word is a palindrome
+    """
+#    print repr(word)
+    if (len(word) == 1)or(len(word) == 0):
+        return True
+    else:
+        if first(word) == last(word):
+            return is_palindrome(middle(word))
+        else:
+            return False
+    
+
 
 
 # Ex 6.8
+
+def gcd(a,b):
+    """
+    Finding the greatest common divisor of a and b without any remainder.
+    """
+#    print repr(a) + " and " + repr(b)
+    if b == 0:
+        return a
+    else:
+        remainder = a%b
+        other = gcd(b,remainder)
+        return other
+
+
+
+
+ 
+a = is_palindrome("wow")    
+print a
+
+b = gcd(8,21)
+print b
+    
+    
--- chap7.py
+++ chap7.py
@@ -1,9 +1,37 @@
-# Enter your answrs for chapter 7 here
-# Name:
+# Enter your answers for chapter 7 here
+# Name: Jenna Gopilan
 
-
+from math import *
 # Ex. 7.5
 
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def estimate_pi():
+    """
+    Based on the formula - calculate an estimate of pi
+    """
+    constant = ((2*(sqrt(2))/9801))
+    summationfinal = 0
+    k = 0
+    while True:
+        is_top = ((factorial(4*k))*(1103 + 26390*k))
+        is_bottom = ((factorial(k)**4)*396**(4*k))
+        term = (1.0 * is_top)/is_bottom
+        summationfinal += term
+        if term < 1e-15:
+#            print k
+            break 
+        k += 1
+    return 1.0/(summationfinal * constant)
+    
+a = estimate_pi()
+print a 
 
 
 # How many iterations does it take to converge?
+
+# It took 4 iterations for it converge.
